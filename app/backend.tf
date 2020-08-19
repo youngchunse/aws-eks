4737d8a5d7a7f316d02605c0518e9561649e1fde
youngchunse <young.chun.se@gmail.com>
2020-08-19 11:41:04 -0400

    creating eks cluster

---
+++ app/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket               = "terraform-state-dev-application"
+    dynamodb_table       = "terraform-state-dev-application-lock"
+    key                  = "dev/us-east-1/eks"
+    region               = "us-east-1"
+  }
+}
---
+++ app/config/dev.tfvars
@@ -0,0 +1,6 @@
+#tags = "dev_app"
+
+cluster_name = "nginx_app"
+#cluster_endpoint_public_access = true
+#cluster_endpoint_private_access = false
+#cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
