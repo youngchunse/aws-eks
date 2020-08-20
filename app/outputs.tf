78f23ce3facad31c9ac585440ab383c465a046b5
youngchunse <young.chun.se@gmail.com>
2020-08-20 09:14:30 -0400

    test

--- app/outputs.tf
+++ app/outputs.tf
@@ -1,21 +1,21 @@
 output "kubeconfig" {
-  value       = module.my-cluster.kubeconfig
+  value       = module.eks.kubeconfig
   description = "EKS Kubeconfig"
 }
 
 output "config_map" {
-  value       = module.my-cluster.config_map_aws_auth
+  value       = module.eks.config_map_aws_auth
   description = "K8S config map to authorize"
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane."
-  value       = module.my-cluster.cluster_endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane."
-  value       = module.my-cluster.cluster_security_group_id
+  value       = module.eks.cluster_security_group_id
 }
 
 output "region" {
